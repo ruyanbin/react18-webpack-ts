8c0d050a67b18bd6f08ecda67195460f9490a948
yanbinru <18303671156@163.com>
2022-12-04 23:54:12 +0800

    webpack 配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
+/*
+ * @Author: yanbinru 18303671156@163.com
+ * @Date: 2022-12-04 22:42:49
+ * @LastEditors: yanbinru 18303671156@163.com
+ * @LastEditTime: 2022-12-04 22:59:20
+ * @FilePath: /react18-webpack-ts/src/App.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { Button } from 'antd';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <Button type="primary">Button</Button>
+    
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
+/*
+ * @Author: yanbinru 18303671156@163.com
+ * @Date: 2022-12-04 22:42:49
+ * @LastEditors: yanbinru 18303671156@163.com
+ * @LastEditTime: 2022-12-04 22:58:53
+ * @FilePath: /react18-webpack-ts/src/index.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'antd/dist/reset.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
